b5f35224d6ea0eb7c73ba1451c6e6cd2936b5028
zdimension <zippedfire@free.fr>
2015-09-13 18:23:52 +0200

    Added HDevelop

---
+++ HDevelop/Src/HDevelop.Core/HSView.xaml.cs
@@ -0,0 +1,58 @@
+﻿#region License
+
+// Copyright (c) 2013 Chandramouleswaran Ravichandran
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+using System;
+using System.IO;
+using System.Windows.Controls;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using Wide.Interfaces;
+using Wide.Interfaces.Services;
+
+namespace HDevelop.Core
+{
+    /// <summary>
+    /// Class HSView
+    /// </summary>
+    public partial class HSView : IContentView
+    {
+        private IStatusbarService _statusbar;
+
+        public HSView(IStatusbarService statusbar)
+        {
+            this._statusbar = statusbar;
+            InitializeComponent();
+            textEditor.TextArea.Caret.PositionChanged += Caret_PositionChanged;
+            using (Stream s = typeof(HSView).Assembly.GetManifestResourceStream("HDevelop.Core.Hassium.xshd"))
+            {
+                using (XmlTextReader reader = new XmlTextReader(s))
+                {
+                    textEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+        }
+
+        private void Caret_PositionChanged(object sender, EventArgs e)
+        {
+            Update();
+        }
+
+        private void Update()
+        {
+            _statusbar.LineNumber = textEditor.Document.GetLineByOffset(textEditor.CaretOffset).LineNumber;
+            _statusbar.ColPosition = textEditor.TextArea.Caret.VisualColumn + 1;
+            _statusbar.CharPosition = textEditor.CaretOffset;
+            _statusbar.InsertMode = false;
+        }
+    }
+}
