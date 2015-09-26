8edab8d644448c22c06498f2065663a79e0b57ac
zdimension <zippedfire@free.fr>
2015-09-27 01:29:46 +0200

    Added dlls

--- HDevelop/Build.bat
+++ HDevelop/Build.bat
@@ -1,5 +1,6 @@
 msbuild Wide.sln /t:clean
 FOR /D %%p IN ("Build\*.*") DO rmdir "%%p" /s /q
 rmdir Build /s /q
-msbuild Wide.sln /p:Configuration=Release
+msbuild HDevelop.sln /p:Configuration=Release
 xcopy "Libs"  "Build\External" /E /Y /I /R
+copy Hassium.exe Build\Hassium.exe
